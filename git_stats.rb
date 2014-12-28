5d42ff05b50bd25ed87630d4ad161bb80eaa0aa2
Duc Thien <ducthien1490@gmail.com>
2014-12-28 17:13:55 +0700

    Initialize git-stats

---
+++ git_stats.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require "optparse"
+require "fileutils"
+require "date"
+require "rubygems"
+require "pry-rails"
+require "./git_stats/author"
+require "./git_stats/yearmonth"
+require "./git_stats/git"
+
+require "./git_stats/stats"
+require "./git_stats/stats/commit"
+require "./git_stats/stats/commit/author"
+require "./git_stats/stats/commit/time"
+require "./git_stats/stats/file"
+require "./git_stats/stats/file/filetype"
+
+require "./git_stats/statgen"
+
+require "./git_stats/renderer"
+require "./git_stats/renderer/haml"
+require "./git_stats/renderer/sass"
+require "./git_stats/renderer/gnuplot"
+
+$options = {
+  out: "stats",
+  verbose: false,
+  start_date: nil,
+  end_date: nil
+}
+
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage git_stats.rb [option] <gitdir> <start date(yyyy/mm/dd)> <end data()>"
+
+  opts.on("-v", "--[no-]verbose", "verbose mode") do |arg|
+    binding.pry
+    $options[:verbose] = arg
+  end
+end
+
+parser.parse!
